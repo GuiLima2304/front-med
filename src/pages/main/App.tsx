bde710d8956da9bd7a53a454910bd9606cf146ec
guilima2304 <gui.lima2304@gmail.com>
2020-10-09 23:04:11 -0300

    integração com o back

--- src/pages/main/App.tsx
+++ src/pages/main/App.tsx
@@ -1,28 +1,90 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import api from '../../services/api';
 
 function App() {
+  
+
+const [formData, setFormData] = useState({
+  primary_drug: '',
+  second_drug: ''
+});
+
+function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+  const { name, value } = event.target;
+  
+  setFormData({ ...formData, [name]: value })
+}
+
+async function handleSubmit(event: FormEvent) {
+  event.preventDefault();
+  
+  const { primary_drug, second_drug } = formData;
+  
+  const data = {
+    primary_drug,
+    second_drug
+  }
+  
+  await api.post('medicines', data)
+    .then(response => setValuesAboutDrugs(response))
+    .catch((error) => { console.log(error) });
+}
+
+var teste1;
+var teste2;
+
+async function setValuesAboutDrugs(response:any) {
+
+  const aboutDrugs = {
+    aboutDrug1: null,
+    aboutDrug2: null
+  };
+
+  console.log(response)
+
+  teste1 = response.data[0];
+  teste2 = response.data[1];
+
+  console.log(teste1)
+
+  aboutDrugs.aboutDrug1 = response[0]
+  aboutDrugs.aboutDrug2 = response[1]
+
+  console.log(aboutDrugs.aboutDrug1)
+}
+
+// const  aboutDrug2  = useState;
+
   return (
     <div className="App">
       <header className="App-header">
+        <form onSubmit={handleSubmit}>
 
         <div className="Form">
           <div>
             <label>1º Medicamento:</label><br />
-            <input />
+            <input type="text"
+                   name="primary_drug"
+                   id="primary_drug"
+                   onChange={handleInputChange}></input>
           </div>
 
           <div>
             <label>2º Medicamento:</label><br />
-            <input />
+            <input type="text"
+                   name="second_drug"
+                   id="second_drug"
+                   onChange={handleInputChange}></input>
           </div>
 
           <div className='actions'>
-            <button>Buscar</button>
+            <button type="submit">Buscar</button>
           </div>
 
         </div>
+          </form>
       </header>
 
       <div className="Interation">
@@ -32,10 +94,12 @@ function App() {
 
       <div className="Bulls">
         <div className="FirstBull">
-          <h3>Primeira Bula</h3>
+          <h3>Primeiro Medicamento</h3>
+          <p>{teste1}</p>
         </div>
         <div className="SecondBull">
-          <h3>Segunda Bula</h3>
+        <h3>Primeiro Medicamento</h3>
+          <p>{teste2}</p>
         </div>
       </div>
 
