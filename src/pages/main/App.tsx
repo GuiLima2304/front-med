70dfecf9c227479fc3b004eaa68545be508b9b32
Padua <padua.matheus13@gmail.com>
2020-10-16 22:59:46 -0300

    acertando os requests

--- src/pages/main/App.tsx
+++ src/pages/main/App.tsx
@@ -1,61 +1,42 @@
 import React, { ChangeEvent, FormEvent, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import api from '../../services/api';
 
-function App() {
-  
+import './App.css';
 
-const [formData, setFormData] = useState({
-  primary_drug: '',
-  second_drug: ''
-});
+const App = () => {
+  
+  const [formData, setFormData] = useState({
+    primary_drug: '',
+    second_drug: ''
+  });
 
-function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
-  const { name, value } = event.target;
-  
-  setFormData({ ...formData, [name]: value })
-}
+  const [resPriDrug, setResPriDrug] = useState('')
+  const [resSecDrug, setResSecDrug] = useState('')
 
-async function handleSubmit(event: FormEvent) {
-  event.preventDefault();
-  
-  const { primary_drug, second_drug } = formData;
-  
-  const data = {
-    primary_drug,
-    second_drug
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value })
   }
-  
-  await api.post('medicines', data)
-    .then(response => setValuesAboutDrugs(response))
-    .catch((error) => { console.log(error) });
-}
 
-var teste1;
-var teste2;
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    
+    const { primary_drug, second_drug } = formData;
+    
+    const data = {
+      primary_drug,
+      second_drug
+    }
+    
+    await api.post('medicines', data)
+      .then(response => setValuesAboutDrugs(response))
+      .catch((error) => { console.log(error) });
+  }
 
-async function setValuesAboutDrugs(response:any) {
-
-  const aboutDrugs = {
-    aboutDrug1: null,
-    aboutDrug2: null
-  };
-
-  console.log(response)
-
-  teste1 = response.data[0];
-  teste2 = response.data[1];
-
-  console.log(teste1)
-
-  aboutDrugs.aboutDrug1 = response[0]
-  aboutDrugs.aboutDrug2 = response[1]
-
-  console.log(aboutDrugs.aboutDrug1)
-}
-
-// const  aboutDrug2  = useState;
+  async function setValuesAboutDrugs(response:any) {
+    setResPriDrug(response.data[0])
+    setResSecDrug(response.data[1])
+  }
 
   return (
     <div className="App">
@@ -95,11 +76,11 @@ async function setValuesAboutDrugs(response:any) {
       <div className="Bulls">
         <div className="FirstBull">
           <h3>Primeiro Medicamento</h3>
-          <p>{teste1}</p>
+          <p>{resPriDrug}</p>
         </div>
         <div className="SecondBull">
         <h3>Primeiro Medicamento</h3>
-          <p>{teste2}</p>
+          <p>{resSecDrug}</p>
         </div>
       </div>
 
