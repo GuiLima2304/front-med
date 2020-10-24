f8b6417eb860a564135b175b11923476621707ed
Padua <padua.matheus13@gmail.com>
2020-10-23 21:28:36 -0300

    print resposne

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Navbar from './components/Navbar';
-import './index.css';
 import App from './pages/App';
 import * as serviceWorker from './serviceWorker';
+
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,173 +1,144 @@
-import React, { ChangeEvent, FormEvent, useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-scroll'
 
-import { TextField, Button } from '@material-ui/core';
+import { Input } from '@material-ui/core';
 
 import api from '../services/api';
 
 const App = () => {
+
+  const [priDrug, setPriDrug] = useState<String>('')
+  const [secDrug, setSecDrug] = useState<String>('')
+
+  const [resPriDrug, setResPriDrug] = useState('')
+  const [resSecDrug, setResSecDrug] = useState('')
+
+  async function handleSubmit(primary_drug: any, second_drug: any) {    
+   
+    const data = {
+      primary_drug,
+      second_drug
+    }    
+    
+     await api.post('medicines', data)
+      .then(res => setValuesAboutDrugs(res))
+      .catch((error) => { console.log(error) });
+   }
+
+   async function setValuesAboutDrugs(response: any) {       
+    
+    setResPriDrug(response.data[0].teste[0].description)
+    setResSecDrug(response.data[1].teste)
+   }
   
   return (
-    <div className="App">
+    <div className='App'>
       
-      <div className={"section" + (false ? " section-dark" : "")}>
-        <div className="section-content" id='intro'>
-          <h1>Olá!</h1>
-          <p>Bem vindo a BulasMed</p>
+      <div className='section'  id='intro'>
+        <div className='section-content'>
+          <h1 className='title'>Olá!</h1>
+          <text className='subtitle'>Bem-vindo a <strong>BulasMed!</strong></text>
 
           <Link
-            activeClass="active"
-            to="section1"
+            activeClass='active'
+            to='section1'
             spy={true}
             smooth={true}
             offset={-70}
             duration={500}>
               
-              <Button variant="contained" color="primary">Começar</Button>
+              <div className='actions'>
+                <button type='submit'>Clique para começar</button>
+              </div>
+          </Link>
+        
+        </div>
+      </div>
+
+      <div className='section' id='section1'>
+        <div className='section-content' >
+          <text className='question'><span className='number'><strong>1 -</strong></span> Insira seu <strong>primeiro medicamento.</strong>*</text>
+          
+          <Input 
+            placeholder="Nome do medicamento..." 
+            id='primary_drug'
+            name='primary_drug'
+            style={{marginBottom: 30, width: 550, fontSize: 23, color: '#1F7FFF'}}
+            onChange={drug => setPriDrug(drug.target.value)}
+            />
+
+          <Link
+            activeClass='active'
+            to='section2'
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}>
+              
+              <div className='actions'>
+                <button>Próximo</button>
+              </div>
             </Link>
         </div>
       </div>
 
-      <div className={"section" + (false ? " section-dark" : "")}>
-        <div className="section-content" id='section1'>
-          <p>Insira seu primeiro medicamento</p>
-          <TextField 
-            id="primary_drug"
-            name="primary_drug"
-            variant="filled"
-            label="Primeiro medicamento" 
-           
+      <div className='section' id='section2'>
+        <div className='section-content' >
+          <text className='question'><span className='number'><strong>2 -</strong></span> Insira seu <strong>segundo medicamento.</strong>*</text>
+          
+          <Input 
+            placeholder="Nome do medicamento..." 
+            id='second_drug'
+            name='second_drug'
+            style={{marginBottom: 30, width: 550, fontSize: 23, color: '#1F7FFF'}}
+            onChange={drug => setSecDrug(drug.target.value)}
           />
 
           <Link
-            activeClass="active"
-            to="intro"
+            activeClass='active'
+            to='section3'
             spy={true}
             smooth={true}
             offset={-70}
             duration={500}>
               
-              <Button variant="contained" color="primary">Próximo</Button>
+              <div className='actions'>
+                <button onClick={() => handleSubmit(priDrug, secDrug)}>Buscar</button>
+              </div>
             </Link>
         </div>
       </div>
 
-      
+      <div className='section' id='section3'>
+        <div className='section-content' >
+        <text className='question'>
+          <span className='number'>
+            <strong>
+              Encontramos isso:
+            </strong>
+          </span>
+            { resPriDrug }
+          </text>
+          
+          <Link
+            activeClass='active'
+            to='response'
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}>
+              
+              <div className='actions'>
+                <button>Nova consulta</button>
+              </div>
+            </Link>
+        </div>
+      </div>
 
-
-
-
-      {/* <Section
-        title="Section 2"
-        subtitle={dummyText}
-        dark={false}
-        id="section2"
-      />
-      <Section
-        title="Section 3"
-        subtitle={dummyText}
-        dark={true}
-        id="section3"
-      />
-      <Section
-        title="Section 4"
-        subtitle={dummyText}
-        dark={false}
-        id="section4"
-      />
-      <Section
-        title="Section 5"
-        subtitle={dummyText}
-        dark={true}
-        id="section5"
-      /> */}
     </div>
   );
 }
 
 export default App;
-
-
-  // const [formData, setFormData] = useState({
-  //   primary_drug: '',
-  //   second_drug: ''
-  // });
-
-  // const [resPriDrug, setResPriDrug] = useState('')
-  // const [resSecDrug, setResSecDrug] = useState('')
-
-  // const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-  //   const { name, value } = event.target;
-  //   setFormData({ ...formData, [name]: value })
-  // }
-
-  // async function handleSubmit(event: FormEvent) {
-  //   event.preventDefault();
-    
-  //   const { primary_drug, second_drug } = formData;
-    
-  //   const data = {
-  //     primary_drug,
-  //     second_drug
-  //   }
-    
-  //   await api.post('medicines', data)
-  //     .then(response => setValuesAboutDrugs(response))
-  //     .catch((error) => { console.log(error) });
-  // }
-
-  // async function setValuesAboutDrugs(response:any) {
-  //   setResPriDrug(response.data[0])
-  //   setResSecDrug(response.data[1])
-  // }
-
-
-
-{/* <div className="App">
-<header className="App-header">
-  <form onSubmit={handleSubmit}>
-
-    <div className="Form">
-      <TextField 
-        id="primary_drug"
-        name="primary_drug"
-        variant="filled"
-        label="Primeiro medicamento" 
-        onChange={handleInputChange}
-      />
-
-      <TextField 
-        id="second_drug"                   
-        name="second_drug"
-        variant="filled"
-        label="Segundo medicamento" 
-        onChange={handleInputChange}
-      />
-    
-      <div className='actions'>
-        <button type="submit">Buscar</button>
-      </div>
-
-    </div>
-  </form>
-</header>
-
-<div className="Interation">
-  <h2>Não há Interação Medicamentosa</h2>
-  <h3>Medicamento X com Medicamento Y não apresentou nenhuma interação medicamentosa</h3>
-</div>
-
-<div className="Bulls">
-  <div className="FirstBull">
-    <h3>Primeiro Medicamento</h3>
-    <p>{resPriDrug}</p>
-  </div>
-  <div className="SecondBull">
-  <h3>Primeiro Medicamento</h3>
-    <p>{resSecDrug}</p>
-  </div>
-</div>
-
-</div> */}
 
 
